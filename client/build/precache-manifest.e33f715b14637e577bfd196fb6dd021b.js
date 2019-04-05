cfc03cc9686b02bc57cc2896e641d3246cd416ba
Rohan Paul <rohanpaul2@gmail.com>
2019-04-05 20:01:15 +0530

    first commit

---
+++ client/build/precache-manifest.e33f715b14637e577bfd196fb6dd021b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e8df429fe1383cf1270",
+    "url": "/static/css/main.a68c2350.chunk.css"
+  },
+  {
+    "revision": "9e8df429fe1383cf1270",
+    "url": "/static/js/main.33d16af2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a44016d0ec4cde7745343094bedab066",
+    "url": "/index.html"
+  }
+];
