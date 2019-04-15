d39c725c0434e338200346cf60e8dc9e908b3021
Rohan Paul <rohanpaul2@gmail.com>
2019-04-15 10:54:29 +0530

    building client file and changing server.js accordingly to server built file from client

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.use((err, req, res, next) => {
   res.status(422).send({ error: err._message });
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Express server listening on ${port} `);
